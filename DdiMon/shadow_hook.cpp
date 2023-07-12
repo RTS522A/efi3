db684392c1f88121395b73817dedb3fcd8f96845
RTS522A <95965159+RTS522A@users.noreply.github.com>
2023-07-12 04:44:54 +0300

    Update shadow_hook.cpp

--- DdiMon/shadow_hook.cpp
+++ DdiMon/shadow_hook.cpp
@@ -18,7 +18,7 @@
 #include <vector>
 #include <memory>
 #include <algorithm>
-#include "../capstoneinclude/capstone/capstone.h"
+#include "../capstone/include/capstone/capstone.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 //
